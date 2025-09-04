7762901edabf9e6d52736fda989c20d4bcbba0ff
Zsomborkkszki <144881316+Zsomborkkszki@users.noreply.github.com>
2025-09-04 13:26:28 +0200

    Python program feltöltés
    Egyenlőre csak ötletek vannak benne

---
+++ app.py
@@ -0,0 +1,23 @@
+'''
+1. Időadat
+
+    Hónap / dátum (pl. 2025-01, 2025-02…)
+
+2. Értékadatok (attól függően, mit elemzel):
+
+    Bevétel (pl. fizetés, hozam, nyereség)
+
+    Kiadás (pl. költségek, veszteség)
+
+    Megtakarítás / egyenleg
+
+    Portfólió aktuális értéke (ha befektetéseket követsz)
+
+3. Opcionális adatok:
+
+    Befektetési típus (részvény, kripto, kötvény, stb.)
+
+    Kategória (pl. fix kiadás, extra bevétel, stb.)
+
+    Jegyzet / megjegyzés
+'''
